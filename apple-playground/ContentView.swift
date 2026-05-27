cab7cd21081bfb5fc06e087a22f6b066eda4d07d
Muukii <muukii.app@gmail.com>
2026-05-27 14:14:03 +0900

    Initial Commit

---
+++ apple-playground/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  apple-playground
+//
+//  Created by Hiroshi Kimura on 2026/05/27.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ apple-playground/apple_playgroundApp.swift
@@ -0,0 +1,17 @@
+//
+//  apple_playgroundApp.swift
+//  apple-playground
+//
+//  Created by Hiroshi Kimura on 2026/05/27.
+//
+
+import SwiftUI
+
+@main
+struct apple_playgroundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
